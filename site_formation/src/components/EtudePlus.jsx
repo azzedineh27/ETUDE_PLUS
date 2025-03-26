ee218015559e11d7b5c68814bc590d5a5e42057b
Azzedine hatem <azzedinehatem@gmail.com>
2025-03-26 18:07:28 +0100

    tests et changement section etude plus

--- site_formation/src/components/EtudePlus.jsx
+++ site_formation/src/components/EtudePlus.jsx
@@ -1,39 +1,41 @@
 import React from "react";
-import "../styles/EtudePlus.css"; 
+import "../styles/EtudePlus.css";
 import formationImage from "../assets/formation_etude_plus.jpg";
-import { useTranslation } from "react-i18next"; // Import du hook i18next
+import { useTranslation } from "react-i18next";
+import { FaBookOpen, FaGraduationCap, FaUserFriends } from "react-icons/fa";
 
 const EtudePlus = () => {
+  const { t } = useTranslation();
 
-  const { t, i18n } = useTranslation(); // Utilisation du hook pour obtenir la fonction changeLanguage
-  
-    // Fonction pour changer la langue
-    const changeLanguage = (lang) => {
-      i18n.changeLanguage(lang);
-    };
-    
+  const points = [
+    t("etudeplus_text_1"),
+    t("etudeplus_text_2"),
+    t("etudeplus_text_3")
+  ];
+
+  const icons = [<FaBookOpen />, <FaGraduationCap />, <FaUserFriends />];
+
   return (
     <section className="etudeplus" id="etudeplus">
       <div className="etudeplus-container">
+        <div className="etudeplus-image">
+          <img src={formationImage} alt="Etude Plus - Nos projets" />
+        </div>
+
         <div className="etudeplus-text">
           <h2>{t("etudeplus_title")}</h2>
           <h3>{t("etudeplus_subtitle")}</h3>
-          <p>
-            {t("etudeplus_text_1")}
-          </p>
-          <p>
-          {t("etudeplus_text_2")}
-          </p>
-          <p>
-          {t("etudeplus_text_3")}
-          </p>
+          <div className="etudeplus-points">
+            {points.map((text, index) => (
+              <div className="etudeplus-point" key={index}>
+                <span className="etudeplus-icon">{icons[index]}</span>
+                <p>{text}</p>
+              </div>
+            ))}
+          </div>
           <a href="https://www.etudeplus.org" className="btn-soutien">
-          {t("etudeplus_button")}
+            {t("etudeplus_button")}
           </a>
-        </div>
-
-        <div className="etudeplus-image">
-          <img src={formationImage} alt="Etude Plus - Nos projets" />
         </div>
       </div>
     </section>
